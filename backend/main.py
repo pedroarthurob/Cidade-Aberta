a54b9e3613e5e7dadc419b25833b7b1eb04540c8
caio_jhonatan_alves_pereira <67547955+DovCaio@users.noreply.github.com>
2026-05-30 15:32:08 +0000

    feat: faz a api com chat do gemini

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 import sqlite3
+from google import genai
+import os
 
 app = FastAPI()
 
@@ -18,13 +20,57 @@ def read_root():
     return {"enterprise": "resultado"}
 
 @app.get("/enterprise/bigger_spent")
-def bigger_spend():
-    return {"enterprise": make_query("SELECT * FROM empenhos ORDER BY amount_empenhado DESC;")}
+def bigger_spend(limit: int = 10, page: int = 1):
+    offset = (page - 1) * limit
+
+    query = f"""
+        SELECT *
+        FROM empenhos
+        ORDER BY amount_empenhado DESC
+        LIMIT {limit} OFFSET {offset}
+    """
+
+    return {"empenhos": make_query(query)}
 
 @app.get("/enterprise/less_spent")
-def bigger_spend():
-    return {"enterprise": make_query("SELECT * FROM empenhos ORDER BY amount_empenhado ASC;")}
+def less_spend(limit: int = 10, page: int = 1):
+    offset = (page - 1) * limit
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+    query = f"""
+        SELECT *
+        FROM empenhos
+        ORDER BY amount_empenhado ASC
+        LIMIT {limit} OFFSET {offset}
+    """
+
+    return {"empenhos": make_query(query)}
+
+@app.get("/enterprises")
+def enterprises(limit: int = 10, page: int = 1):
+    offset = (page - 1) * limit
+
+    query = f"""
+        SELECT *
+        FROM empenhos
+        ORDER BY id ASC
+        LIMIT {limit} OFFSET {offset}
+    """
+
+    return {"empenhos": make_query(query)}
+
+@app.get("/chat")
+def chat(prompt: str=""):
+    client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+    if len(prompt) == 0:
+        return {"message" : "Nenhum prompt foi enviado."}
+
+    def chat_with_gemini(prompt: str):
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt
+        )
+        return response.text
+    return {"result" : chat_with_gemini(prompt)}
+
+
